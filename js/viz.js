2c06d6e73169bbd82922ab427fd418a36f7cc4f6
Tyler Yep <tyep@stanford.edu>
2020-05-30 16:29:12 -0700

    code

---
+++ js/viz.js
@@ -0,0 +1,12 @@
+// import interact from 'interactjs';
+
+function main() {
+    const TYLER_IS_THE_BEST = true;
+    for (let i = 0; i < 2; i++) {
+        console.log("Heidi is l o v e l y and " + TYLER_IS_THE_BEST);
+    }
+
+}
+
+
+main()
