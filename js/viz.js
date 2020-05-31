9a838dbf3cf53a04b06192f05bf0faadfc068f3b
Heidi <heidichen7@gmail.com>
2020-05-31 16:18:03 -0700

    broken star

--- js/viz.js
+++ js/viz.js
@@ -1,12 +1,13 @@
-// import interact from 'interactjs';
 
 function main() {
     const TYLER_IS_THE_BEST = true;
-    for (let i = 0; i < 2; i++) {
-        console.log("Heidi is l o v e l y and " + TYLER_IS_THE_BEST);
+    for (let i = 0; i < 3; i++) {
+        console.log("Heidi! is l o v e l y and " + TYLER_IS_THE_BEST);
     }
 
 }
 
 
 main()
+
+// import interact from 'interactjs';
